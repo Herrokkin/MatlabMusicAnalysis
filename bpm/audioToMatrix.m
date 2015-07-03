29e881c1790810746fa6bbde0ee4a033378c74ee
Herrokkin <k1@herrokkin.jp>
2015-07-03 12:16:24 +0900

    Search first peak

--- bpm/audioToMatrix.m
+++ bpm/audioToMatrix.m
@@ -22,23 +22,33 @@ else %csv存在しない場合=>フーリエ変換
     
     %プリエンファシス(高域強調)
     pre_emphasis = 0.97; %プリエンファシス係数
-    merge_emphasis = [];
-    merge_emphasis(1) = merge(1);
+    merge_high_emphasis = [];
+    merge_high_emphasis(1) = merge(1);
     for countData = 2 : 1 : length(merge)
         %プリエンファシス
         thisData = merge(countData) - (pre_emphasis * merge(countData - 1));
-        merge_emphasis = [merge_emphasis; thisData];
+        merge_high_emphasis = [merge_high_emphasis; thisData];
     end
-
+    
+    %拍の先頭を推定
+    peak_count_frame_size = 512;
+    peak_count_frame_max = 44100 * 5 / peak_count_frame_size; %最初のn秒間を使用
+    for peak_count = 1 : peak_count_frame_max - 1
+        if sum(merge_high_emphasis((peak_count - 1) * peak_count_frame_size + 1 : (peak_count - 1) * peak_count_frame_size + peak_count_frame_size, 1)) .^2 <= sum(merge_high_emphasis(peak_count * peak_count_frame_size + 1 : peak_count * peak_count_frame_size + peak_count_frame_size, 1)) .^2 && sum(merge_high_emphasis(peak_count * peak_count_frame_size + 1 : peak_count * peak_count_frame_size + peak_count_frame_size, 1)) .^2 >= sum(merge_high_emphasis((peak_count + 1) * peak_count_frame_size + 1 : (peak_count + 1) * peak_count_frame_size + peak_count_frame_size, 1)) .^2
+            peak_count_start = peak_count;
+        end
+    end
+    merge_starttime = merge_high_emphasis(peak_count_start * peak_count_frame_size : length(merge_high_emphasis),1);
+    
     %高速フーリエ変換
     frame_length = floor(Fs / (songBPM / 60) * 8); %フレーム幅
-    N = floor(length(merge_emphasis) / frame_length); %楽曲÷フレーム幅
+    N = floor(length(merge_starttime) / frame_length); %楽曲÷フレーム幅
     width_result = Fs; %サンプリング長
     result = zeros(N, width_result); %返り値設定
     window = hamming(width_result); %ハミング窓設定
     index = 1;
     for t = 1 : N
-        frame = merge_emphasis( (t - 1) * frame_length + 1 : (t - 1) * frame_length + width_result, 1); %フレーム設定
+        frame = merge_starttime( (t - 1) * frame_length + 1 : (t - 1) * frame_length + width_result, 1); %フレーム設定
         frame_window = frame .* window; %ハミング窓で丸め
         spectrum = abs(fft(frame_window)); %高速フーリエ変換でスペクトラム生成
 %         result(index, :) = spectrum(1:width_result, :); %秒ごとにスペクトラムを記録。
